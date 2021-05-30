fd5c2a8e42d5ad80acd4c131dfacfd5470a7c003
Gustavo F Reis, Nukkle <55332994+Nukkle134@users.noreply.github.com>
2021-05-30 05:29:21 -0300

    Add files via upload
    <h1>Treino para json.</h1>
    <br>> Puxar os valores externos do .JSON e printar no terminar.</br>
    <br>!! Precisa do Newtonsoft.Json.</br>
    <br>Example Output: </br>
    <br>Usuario: Isadora  || Cor: Azul  || Coords: x:1, y:6, z: 2, || Tipo:Circle-Dot</br>
    <br>Usuario: Mariana || Cor: Anil  || Coords: x:1, y:6, z: 2, || Tipo:Quad-Dot</br>
    <br>Usuario: Maria--  || Cor: Rosa || Coords: x:1, y:6, z: 2, || Tipo:Quad-Dot</br>

---
+++ Treino - Coletar valores de um .JSON e listá-los/json_tests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace testes_json
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var getConfig = new JObject();
+            var config = new JObject();
+            var configPath = @"C:\Users\nukkle\RiderProjects\testes_json\testes_json\Config.json";
+            if (File.Exists(configPath))
+            {
+                var readFile = File.ReadAllText(configPath, Encoding.UTF8);
+                var reader = JObject.Parse(readFile);
+                foreach (var users in reader["Blips"].Children<JObject>())
+                foreach (var cat in users.Properties())
+                {
+                    var x = cat.Value[0]["Coords"][0];
+                    var y = cat.Value[0]["Coords"][1];
+                    var z = cat.Value[0]["Coords"][2];
+                    var cor = cat.Value[0]["Cor"];
+                    var type = cat.Value[0]["Type"];
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(
+                        $"Usuario:{cat.Name} || Cor: {cor} || Coords: x:{x}, y:{y}, z: {z}, || Tipo:{type}");
+                }
+            }
+        }
+    }
+}
